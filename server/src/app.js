d72d3721c5cfb9ff8b6ae3d7de144a132011efd0
CGomezGomez <cggomez93@gmail.com>
2023-07-20 13:54:24 +0200

    .actualizado

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 // Rutas
 
 // Middlewares para cliente
+
 app.use(cors());
+
 app.use(express.json());
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('Base de datos conectada');
+  } catch (err) {
+    console.error('No se ha podido iniciar la base de datos');
+  }
+
+  app.listen(process.env.PORT, () =>
+    console.log('Servidor en ejecución en el puerto 3000')
+  );
+};
 
 // Uso de rutas
 
-app.listen(3000, () => console.log('Servidor en ejecución en el puerto 3000'));
+startServer();
